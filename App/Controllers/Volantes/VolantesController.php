af5ef33b569064b3e1ee7813a90a4234f8a75cb8
soujiro27 <jc.raus27@gmail.com>
2018-08-24 19:01:08 +0100

    volantes terminados

--- App/Controllers/Volantes/VolantesController.php
+++ App/Controllers/Volantes/VolantesController.php
@@ -61,7 +61,6 @@ class VolantesController extends TwigController {
 		echo $this->render('HomeLayout/UpdateContainer.twig',$data);
 	}
 
-
 /*-------------------Tabla-----------------------------*/
 
 	public function tabla(){
